330f10bc6e2c9c043273103e47a3f27a6af62de0
sasa <svetasmak0@gmail.com>
2023-12-11 23:36:42 +0100

    Main function for game

--- script.js
+++ script.js
@@ -4,3 +4,37 @@ modalBtn.addEventListener("click", () => {
   modalBtn.style.display = "none";
   overlay.style.display = "none";
 });
+const heading = document.querySelector(".scoreboard-title");
+// LOGIC FOR GAME
+function initGame() {
+  let playerScore = 0;
+  let computerScore = 0;
+  let winner = "";
+
+  const getPlayerScore = () => playerScore;
+  const getComputerScore = () => computerScore;
+
+  const IncrementPlayerScore = () => (playerScore += 1);
+  const IncrementPlayerComputer = () => (computerScore += 1);
+
+  const setWinner = (winner) => {
+    console.log(`${winner} won.`);
+  };
+
+  return {
+    getPlayerScore,
+    getComputerScore,
+    IncrementPlayerComputer,
+    IncrementPlayerScore,
+    setWinner,
+  };
+}
+const game = initGame();
+game.IncrementPlayerScore();
+heading.textContent = game.getPlayerScore();
+
+let playerWinner = document.querySelector(".player-score");
+let computerWinner = document.querySelector(".computer-score");
+console.log(playerWinner, computerWinner);
+
+game.setWinner("player");
